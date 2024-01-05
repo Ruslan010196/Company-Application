cfe558a111b1c4d83d4313c0107efdaf9c550874
unknown <ruslanaab@code.edu.az>
2024-01-05 23:38:56 +0200

    bitirdiyimi yukledim

---
+++ CompanyAplication/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CompanyAplication.Helper;
+
+Helper.ChangeTextColor(ConsoleColor.DarkRed, "CompanyAplication");
+Helper.ChangeTextColor(ConsoleColor.Blue, "Enter number:"+
+    "1.CreateEmployee 2.GetAllEmployee 0.ExitMenu");
+
+EnterMenu: string menu =Console.ReadLine();
+bool result=int.TryParse(menu, out int intMenu);
+while(true)
+if (result&&intMenu>0&&intMenu<3)
+{
+
+
+}
+else if (intMenu == 0)
+{
+        Helper.ChangeTextColor(ConsoleColor.Yellow, "Bye Bye");
+        break;
+}
+else
+{
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Duzgun eded daxil et");
+        goto EnterMenu;
+
+}
